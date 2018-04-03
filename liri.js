c11e99af14dd4b9b9eb0ffeeeb733665c46519d1
Roberto Ayala <rayala26@gmail.com>
2018-04-02 21:56:24 -0400

    created switch statement to declare action to execute.

--- liri.js
+++ liri.js
@@ -5,3 +5,30 @@ var spotify = require('spotify');
 var request = require('request');
 var fs = require('fs');
 
+console.log('Type my-tweets , spotify-this-song , movie-this , or do-what-it-says to get started!');
+
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
+    for (i=4; i <process.argv.length; i++) {
+        secondCommand += '+' + process.argv[i];    
+    }
+
+function theSwitch(){
+    switch(userCommand){
+        case 'my-tweets':
+        fetchTweets();
+        break;
+
+        case 'spotify-this-song':
+        spotifyME();
+        break;
+
+        case 'movie-this':
+        aMovieForMe();
+        break;
+
+        case 'do-what-it-says':
+        followTheTextbook();
+        break;
+    }
+};
